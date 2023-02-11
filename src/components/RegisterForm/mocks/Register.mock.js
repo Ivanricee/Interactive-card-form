6673ca15656d5b2352466917d6337949b50dee50
Iván Bautista <ivanbautistag@gmail.com>
2023-02-11 17:53:51 -0600

    Mock  from values, number with spaces

--- src/components/RegisterForm/mocks/Register.mock.js
+++ src/components/RegisterForm/mocks/Register.mock.js
@@ -1,12 +1,12 @@
 export const registerFormMock = {
   name: 'Ivanrice',
-  number: '25856489633891017',
+  number: '2585 6489 6338 9101',
   month: '09',
   year: '00',
   cvc: '123',
 }
 export const registerFormError = {
-  number: '9591648963389101E',
+  number: '9591 6489 6338 910E',
 }
 export const registerFormMessageError = {
   empty: "Can't be blank",
