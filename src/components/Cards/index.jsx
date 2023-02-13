313568b28d2b5eec8a95d023f59b0f6057fbc9fc
Iván Bautista <ivanbautistag@gmail.com>
2023-02-13 13:41:11 -0600

    Add cards wrappers avoiding hidden absolute positioned elements

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -17,30 +17,34 @@ export function Cards() {
   })
   return (
     <section className="card">
-      <picture>
-        <source srcSet="/bg-main-desktop.png" media="(min-width: 376px)" />
-        <img src="/bg-main-mobile.png" alt="background cards" />
-      </picture>
-      <div className="card__front">
-        <CardLogo className="card__front-logo" />
-        <div className="card__front-text">
-          <p>{formData.number || initData.current.number}</p>
-          <p>{formData.name || initData.current.name}</p>
-          <p>
-            {formData.month || initData.current.month}/
-            {formData.year || initData.current.year}
-          </p>
-        </div>
-        <div className="card__front-img">
-          <img alt="front card" src={front} width="300px"></img>
-        </div>
+      <div className="card-background">
+        <picture>
+          <source srcSet="/bg-main-desktop.png" media="(min-width: 751px)" />
+          <img src="/bg-main-mobile.png" alt="background cards" />
+        </picture>
       </div>
-      <div className="card__back">
-        <div className="card__back-text">
-          <p>{formData.cvc}</p>
+      <div className="card-wrapper">
+        <div className="card__front">
+          <CardLogo className="card__front-logo" />
+          <div className="card__front-text">
+            <p>{formData.number || initData.current.number}</p>
+            <p>{formData.name || initData.current.name}</p>
+            <p>
+              {formData.month || initData.current.month}/
+              {formData.year || initData.current.year}
+            </p>
+          </div>
+          <div className="card__front-img">
+            <img alt="front card" src={front} width="300px"></img>
+          </div>
         </div>
-        <div className="card__back-img" alt="back card">
-          <img alt="back card" width="288px" src={back}></img>
+        <div className="card__back">
+          <div className="card__back-text">
+            <p>{formData.cvc}</p>
+          </div>
+          <div className="card__back-img" alt="back card">
+            <img alt="back card" width="288px" src={back}></img>
+          </div>
         </div>
       </div>
     </section>
