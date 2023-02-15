367d878772793ffba55c402c1d4f8a9beaaa9871
Iván Bautista <ivanbautistag@gmail.com>
2023-02-14 20:38:49 -0600

    Add form validations

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -27,22 +27,39 @@ export function RegisterForm() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="name">CARDHOLDER NAME</label>
           <input
+            className={errors?.name ? 'input-error' : 'input'}
             type="text"
             aria-label="name"
+            maxLength="40"
             placeholder="e.g. Jane Appleseed"
-            {...register('name', { required: true, onChange })}
+            {...register('name', {
+              required: true,
+              pattern: /^[a-zA-Z\s]+$/,
+              maxLength: 40,
+              onChange,
+            })}
           ></input>
-          {errors.name && <p>Can't be blank</p>}
-
+          {errors.name && errors.name?.type === 'required' && (
+            <p>Can't be blank</p>
+          )}
+          {errors.name && errors.name?.type === 'pattern' && (
+            <p>Wrong format, letters only</p>
+          )}
+          {errors.name && errors.name?.type === 'maxLength' && (
+            <p>Wrong, name too long</p>
+          )}
           <label htmlFor="number">CARD NUMBER</label>
           <input
+            className={errors?.number ? 'input-error' : 'input'}
             value={formData.number}
             type="text"
             aria-label="number"
+            maxLength="19"
             placeholder="e.g. 1234 5678 9123 0000"
             {...register('number', {
               required: true,
               pattern: /^[\d\s]+$/,
+              minLength: 19,
               onChange,
             })}
           ></input>
@@ -52,32 +69,69 @@ export function RegisterForm() {
           {errors.number && errors.number?.type === 'pattern' && (
             <p>Wrong format, numbers only</p>
           )}
+          {errors.number && errors.number?.type === 'minLength' && (
+            <p>Wrong card number</p>
+          )}
           <div className="register-form-inline">
             <div className="register-form-date">
               <label>EXP. DATE (MM/YY)</label>
               <input
+                className={errors?.month ? 'input-error' : 'input'}
                 type="text"
                 aria-label="month"
                 placeholder="MM"
-                {...register('month', { required: true, onChange })}
+                maxLength="2"
+                {...register('month', {
+                  required: true,
+                  pattern: /^(0?[1-9]|1[012])$/,
+                  minLength: 2,
+                  onChange,
+                })}
               ></input>
               <input
+                className={errors?.year ? 'input-error' : 'input'}
                 type="text"
                 aria-label="year"
                 placeholder="YY"
-                {...register('year', { required: true, onChange })}
+                maxLength="2"
+                {...register('year', {
+                  required: true,
+                  pattern: /^[0-9]{2}$/,
+                  minLength: 2,
+                  onChange,
+                })}
               ></input>
-              {(errors.month || errors.year) && <p>Can't be blank</p>}
+              {(errors.month?.type === 'required' ||
+                errors.year?.type === 'required') && <p>Can't be blank</p>}
+              {(errors.month?.type === 'pattern' ||
+                errors.year?.type === 'pattern') && <p>Wrong format</p>}
+              {(errors.month?.type === 'minLength' ||
+                errors.year?.type === 'minLength') && <p>Wrong date</p>}
             </div>
             <div className="register-form-cvc">
               <label htmlFor="cvc">CVC</label>
               <input
+                className={errors?.cvc ? 'input-error' : 'input'}
                 type="text"
                 aria-label="cvc"
                 placeholder="e.g. 123"
-                {...register('cvc', { required: true, onChange })}
+                maxLength="3"
+                {...register('cvc', {
+                  required: true,
+                  pattern: /^[0-9]{3}$/,
+                  minLength: 3,
+                  onChange,
+                })}
               ></input>
-              {errors.cvc && <p>Can't be blank</p>}
+              {errors.cvc && errors.cvc?.type === 'required' && (
+                <p>Can't be blank</p>
+              )}
+              {errors.cvc && errors.cvc?.type === 'pattern' && (
+                <p>Wrong format, numbers only</p>
+              )}
+              {errors.cvc && errors.cvc?.type === 'minLength' && (
+                <p>Wrong cvc</p>
+              )}
             </div>
           </div>
           <button type="submit">Confirm</button>
