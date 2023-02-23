9f0b4ff3d93b86dcb097d124ef18139b1928fbb3
Iván Bautista <ivanbautistag@gmail.com>
2023-02-23 14:59:08 -0600

    Refactor context success state when form is valid reset form.

--- src/components/RegisterForm/Success/index.jsx
+++ src/components/RegisterForm/Success/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Complete } from '../../Icon'
 import './styles.css'
+import { useFormValuesContext } from '../../../context/FormContext'
+
 export function Success() {
+  const [_, { setSuccessForm }] = useFormValuesContext()
+
+  const handleClick = () => {
+    setSuccessForm(false)
+  }
   return (
     <section>
       <Complete className="register-form--success" />
       <h1>THANK YOU!</h1>
       <p>We've added your card details</p>
-      <button type="submit">Confirm</button>
+      <button type="button" onClick={handleClick}>
+        Confirm
+      </button>
     </section>
   )
 }
